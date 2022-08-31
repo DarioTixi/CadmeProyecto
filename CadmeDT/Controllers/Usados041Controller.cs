3cf755cc8d2b6f79de96bb009a347bbc106d53ab
DarioTixi <dario.moya2016@gmail.com>
2022-08-31 14:17:23 -0500

    Guardar

---
+++ CadmeDT/Controllers/Usados041Controller.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using CadmeDT.Models;
+using System.Data;
+using System.Data.Entity.Validation;
+
+namespace CadmeDT.Controllers
+{
+    public class Usados041Controller : Controller
+    {
+        //Listar los elementos de las tablas
+        // GET: Usados041
+        public ActionResult Index()
+        {
+            List<usados041> usados041 = new List<usados041>();
+            using (cadmeEntities cadme = new cadmeEntities())
+            {
+                usados041 = cadme.usados041.ToList();
+            }
+            return View(usados041);
+        }
+        //Esta es la opcion de detalles de un solo campo
+        // GET: Usados041/Details/5
+        public ActionResult Details(int id)
+        {
+            usados041 usados041 = new usados041();
+            using (cadmeEntities cadme = new cadmeEntities())
+            {
+                usados041 = cadme.usados041.Where(x => x.id == id).FirstOrDefault();
+            }
+
+            return View(usados041);
+        }
+
+        // GET: Usados041/Create
+        //Crear la nueva vista con todos los atributos de la tabla
+        public ActionResult Create()
+        {
+            return View( new usados041());
+        }
+
+        // POST: Usados041/Create
+        //Metodo para agregar los elementos que deseamos a las tablas
+        [HttpPost]
+        public ActionResult Create(usados041 usados041)
+        {
+            using (cadmeEntities cadme = new cadmeEntities())
+            {
+                cadme.usados041.Add(usados041);
+                cadme.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Usados041/Edit/5
+        public ActionResult Edit(int id)
+        {
+            usados041 usados041 = new usados041();
+            using (cadmeEntities cadme = new cadmeEntities())
+            {
+                usados041 = cadme.usados041.Where(x => x.id == id).FirstOrDefault();
+            }
+
+            return View(usados041);
+        }
+        //Este es el que en verdad guarda cuando se le edita 
+        // POST: Usados041/Edit/5
+        [HttpPost]
+        public ActionResult Edit(usados041 usados041)
+        {
+
+            try
+            {
+                using (cadmeEntities cadme = new cadmeEntities())
+                {
+                    cadme.Entry(usados041).State = EntityState.Modified;
+                    cadme.SaveChanges();
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Usados041/Delete/5
+        //Vista para borrar 
+        public ActionResult Delete(int id)
+        {
+            usados041 usados041 = new usados041();
+            using (cadmeEntities cadme = new cadmeEntities())
+            {
+                usados041 = cadme.usados041.Where(x => x.id == id).FirstOrDefault();
+            }
+
+            return View(usados041);
+        }
+
+        // POST: Usados041/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            using (cadmeEntities cadme = new cadmeEntities())
+            {
+                usados041 usados041 = cadme.usados041.Where(x => x.id == id).FirstOrDefault();
+                cadme.usados041.Remove(usados041);
+                cadme.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+        
+    }
+    }
+}
---
+++ CadmeDT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CadmeDT.MvcApplication" Language="C#" %>
